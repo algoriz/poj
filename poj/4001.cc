81c75465106c00805de75864ee7f16ec09500c28
algoriz <riz@algonote.com>
2014-07-29 15:06:38 +0800

    POJ 4001 accepted
    Add several test cases for POJ 4001
    Removed input/output files from VS solution

--- poj/4001.cc
+++ poj/4001.cc
@@ -1,8 +1,7 @@
 #include <iostream>
 
-
 struct point {
-    int x, y;
+    int y, x;
 };
 
 std::istream& operator>> (std::istream& is, point& p){
@@ -14,112 +13,31 @@ public:
     enum roles { GENERAL = 'G', ROOK = 'R', HORSE = 'H', CANNON = 'C' };
 
     chinese_chess(){
-        /* board boundary */
-        for (int i = 0; i < 11; ++i){
-            _state[0][i] = 1;
-            _state[11][i] = 1;
-        }
-        for (int i = 0; i < 12; ++i){
-            _state[i][0] = 1;
-            _state[i][10] = 1;
-        }
         reset();
     }
 
     void reset() {
         _n = 0;
-        for (int i = 1; i < 11; ++i){
-            for (int j = 1; j < 10; ++j){
-                _state[i][j] = 0;
-            }
-        }
+        memset(_board, 0, sizeof(_board));
     }
 
     void add_piece(int role, point p){
-        _state[p.y][p.x] = role;
+        _board[p.y][p.x] = role;
         _pieces[_n++] = p;
     }
 
     /* Test if the black general at bgp is checkmated */
     bool is_checkmate(point bgp){
         int checked[12][11];
-        memcpy(checked, _state, sizeof(checked));
-
-        for (int i = 0; i < _n; ++i){
-            point& p = _pieces[i];
-            int x = p.x;
-            int y = p.y;
-            
-            switch (_state[y][x]) {
-            case GENERAL:
-                /* possible flying general */
-                for (int f = v_prev(p), k = 3; k > f; --k){
-                    checked[k][x] = 1;
-                }
-                break;
-            
-            case ROOK:
-                for (int yl = v_prev(p) + 1, yu = v_next(p); yl < yu; ++yl){
-                    checked[yl][x] = 1;
-                }
-                for (int xl = h_prev(p) + 1, xu = h_next(p); xl < xu; ++xl){
-                    checked[y][xl] = 1;
-                }
-                break;
-            
-            case HORSE:{
-                    point hp[8] = {
-                            { x - 1, y - 2 }, { x + 1, y - 2 },
-                            { x + 2, y - 1 }, { x + 2, y + 1 },
-                            { x + 1, y + 2 }, { x - 1, y + 2 },
-                            { x - 2, y + 1 }, { x - 2, y - 1 }
-                    };
-                    point hh[8] = {
-                            { 0, -1 }, { 0, -1 },
-                            { 1, 0 }, { 1, 0 },
-                            { 0, 1 }, { 0, 1 },
-                            { -1, 0 }, { -1, 0 }
-                    };
-                    for (int i = 0; i < 8; ++i){
-                        checked[hp[i].y][hp[i].x] = is_valid(hp[i]) && !_state[y + hh[i].y][x + hh[i].x];
-                    }
-                }
-                break;
-            
-            case CANNON:
-            {
-                point pp = { x, v_prev(p) };
-                point ppp = { x, v_prev(ppp) };
-                for (int k = ppp.y + 1; k < pp.y; ++k){
-                    checked[k][x] = 1;
-                }
-            }
-
-                {
-                    point pp = { x, v_next(p) };
-                    point ppp = { x, v_next(pp) };
-                    for (int k = pp.y + 1; k < ppp.y; ++k){
-                        checked[k][x] = 1;
-                    }
-                }
-
-                {
-                    point pp = { x, h_prev(p) };
-                    point ppp = { x, h_prev(pp) };
-                    for (int k = ppp.x + 1; k < pp.x; ++k){
-                        checked[y][k] = 1;
-                    }
-                }
-
-                {
-                    point pp = { x, h_next(p) };
-                    point ppp = { x, h_next(pp) };
-                    for (int k = pp.x + 1; k < ppp.x; ++k){
-                        checked[y][k] = 1;
-                    }
-                }
-                break;
-            }
+        memset(checked, 0, sizeof(checked));
+        /* board boundary */
+        for (int i = 0; i < 11; ++i){
+            checked[0][i] = 1;
+            checked[11][i] = 1;
+        }
+        for (int i = 0; i < 12; ++i){
+            checked[i][0] = 1;
+            checked[i][10] = 1;
         }
 
         /* No move out of palace */
@@ -133,7 +51,93 @@ public:
         checked[2][7] = 1;
         checked[3][7] = 1;
 
-        return checked[bgp.y - 1][bgp.x] && checked[bgp.y + 1][bgp.x]
+        for (int i = 0; i < _n; ++i){
+            point& p = _pieces[i];
+            int x = p.x;
+            int y = p.y;
+            
+            switch (_board[y][x]) {
+            case GENERAL:
+                /* possible flying general */
+                for (int f = v_prev(p), k = 3; k >= f; --k){
+                    checked[k][x] = 1;
+                }
+                break;
+            
+            case ROOK:
+                for (int yp = v_prev(p); yp != y; ++yp){
+                    checked[yp][x] = 1;
+                }
+                for (int yp = v_next(p); yp != y; --yp){
+                    checked[yp][x] = 1;
+                }
+                for (int xp = h_prev(p); xp != x; ++xp){
+                    checked[y][xp] = 1;
+                }
+                for (int xp = h_next(p); xp != x; --xp){
+                    checked[y][xp] = 1;
+                }
+                break;
+            
+            case HORSE:{
+                    point hp[8] = {
+                            { y - 2, x - 1 }, { y - 2, x + 1 },
+                            { y - 1, x + 2 }, { y + 1, x + 2 },
+                            { y + 2, x + 1 }, { y + 2, x - 1 },
+                            { y + 1, x - 2 }, { y - 1, x - 2 }
+                    };
+                    point hh[8] = {
+                            { -1, 0 }, { -1, 0 },
+                            { 0, 1 }, { 0, 1 },
+                            { 1, 0 }, { 1, 0 },
+                            { 0, -1 }, { 0, -1 }
+                    };
+                    for (int i = 0; i < 8; ++i){
+                        if (is_valid(hp[i]) && !_board[y + hh[i].y][x + hh[i].x]){
+                            checked[hp[i].y][hp[i].x] = 1;
+                        }
+                    }
+                }
+                break;
+            
+            case CANNON:
+                {
+                    point pp = { v_prev(p), x };
+                    point ppp = { v_prev(pp), x };
+                    for (int k = ppp.y; k != pp.y; ++k){
+                        checked[k][x] = 1;
+                    }
+                }
+
+                {
+                    point pp = { v_next(p), x };
+                    point ppp = { v_next(pp), x };
+                    for (int k = ppp.y; k != pp.y; --k){
+                        checked[k][x] = 1;
+                    }
+                }
+
+                {
+                    point pp = { y, h_prev(p) };
+                    point ppp = { y, h_prev(pp) };
+                    for (int k = ppp.x; k != pp.x; ++k){
+                        checked[y][k] = 1;
+                    }
+                }
+
+                {
+                    point pp = { y, h_next(p) };
+                    point ppp = { y, h_next(pp) };
+                    for (int k = ppp.x; k != pp.x; --k){
+                        checked[y][k] = 1;
+                    }
+                }
+                break;
+            }
+        }
+
+        return checked[bgp.y][bgp.x]
+            && checked[bgp.y - 1][bgp.x] && checked[bgp.y + 1][bgp.x]
             && checked[bgp.y][bgp.x + 1] && checked[bgp.y][bgp.x - 1];
     }
 
@@ -144,7 +148,7 @@ private:
 
     int h_next(point p){
         for (int x = p.x + 1; x < 11; ++x){
-            if (_state[p.y][x] != 0){
+            if (_board[p.y][x] != 0){
                 return x;
             }
         }
@@ -153,7 +157,7 @@ private:
 
     int h_prev(point p){
         for (int x = p.x - 1; x > 0; --x){
-            if (_state[p.y][x] != 0){
+            if (_board[p.y][x] != 0){
                 return x;
             }
         }
@@ -161,8 +165,8 @@ private:
     }
 
     int v_next(point p){
-        for (int y = p.x + 1; y < 10; ++y){
-            if (_state[y][p.x] != 0){
+        for (int y = p.y + 1; y < 10; ++y){
+            if (_board[y][p.x] != 0){
                 return y;
             }
         }
@@ -170,8 +174,8 @@ private:
     }
 
     int v_prev(point p){
-        for (int y = p.x - 1; y > 0; --y){
-            if (_state[y][p.x] != 0){
+        for (int y = p.y - 1; y > 0; --y){
+            if (_board[y][p.x] != 0){
                 return y;
             }
         }
@@ -179,7 +183,7 @@ private:
     }
 
 private:
-    int _state[12][11];
+    int _board[12][11];
     point _pieces[8]; /* Red */
     int _n;
 };
